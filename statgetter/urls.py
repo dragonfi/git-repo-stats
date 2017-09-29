b602f242fc8143fec6d63992a92f7c8619bade42
David Gabor Bodor <david.gabor.bodor@gmail.com>
2017-09-29 09:38:13 +0200

    statgetter: add dummy stats/ endpoint

--- statgetter/urls.py
+++ statgetter/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^stats/(?P<repo_url>.*)', views.stats, name = 'stats'),
 ]
--- statgetter/views.py
+++ statgetter/views.py
@@ -7,3 +7,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the statgetter index.")
+
+def stats(request, repo_url):
+    context = {
+        'repo_url': repo_url,
+    }
+    return render(request, 'statgetter/stats.html', context)
